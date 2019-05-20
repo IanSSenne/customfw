840ce6fef5b6e90df9e7dd49464c501784925614
IanSSenne <IanSSenne@gmail.com>
2019-05-19 17:51:02 -0700

    added babel for backwords support and postcss. changes to inline external css.

--- tool/src/build.js
+++ tool/src/build.js
@@ -1,9 +1,28 @@
+const babel = require("@babel/core");
 const fs = require("fs");
 const getFiles = require("get-files-in-dir");
 const path = require("path");
-const cleanCSS = require("clean-css");
+const postcss = require("postcss");
+const postcss_autoprefixer = require("autoprefixer");
+const postcss_handle_imports = require("postcss-import");
 const html = require("jsdom");
 const uglifyes = require("uglify-es");
+const hash = require("./hash.js");
+const fetch = require("node-fetch");
+const file = require("file-url");
+
+function minify(code) {
+  let res = babel.transformSync(`{${code}}`, {
+    presets: ["@babel/preset-env"],
+    plugins: [],
+    comments: false
+  });
+  return minifyes(res.code);
+}
+
+function minifyes(code) {
+  return uglifyes.minify(code).code;
+}
 
 function ends(str, part) {
   return str.lastIndexOf(part) === (str.length - part.length);
@@ -26,7 +45,7 @@ for (let i = 0; i < parts.length; i++) {
 package.version = parts.reverse().join(".");
 fs.writeFileSync(path.resolve(process.cwd(), "./customfw.json"), JSON.stringify(package, null, 2));
 const pack0 = fs.readFileSync(path.resolve(__dirname, "./pack.0.js"), "utf8");
-module.exports.start = function () {
+module.exports.start = async function () {
 
   let n = 0;
   console.log("starting build...");
@@ -40,50 +59,86 @@ module.exports.start = function () {
   final_js += `app_state.set("manifest",${JSON.stringify(package)});\n`;
   for (let i = 0; i < file_list.length; i++) {
     let content = fs.readFileSync(file_list[i]).toString();
-    const dom = new html.JSDOM(content);
-    Array.from(dom.window.document.querySelectorAll("style")).forEach(el => {
-      el.innerHTML = new cleanCSS({}).minify(el.innerHTML).styles;
+    const dom = new html.JSDOM(content, {
+      url: file(file_list[i])
     });
+    let promise = [];
+    Array.from(dom.window.document.querySelectorAll("link")).forEach(async (el) => promise.push((async el => {
+      // el.innerHTML = new cleanCSS({}).minify(el.innerHTML).styles;
+      // el.innerHTML = postcss([postcss_autoprefixer]).process(el.innerHTML);
+      if (el.getAttribute("rel") == "stylesheet") {
+        try {
+          const url = new URL(el.getAttribute("href"));
+          console.log("fetching", url.href);
+          promise.push(fetch(url.href).then(data => data.text()).then(css => {
+            console.log("got", url.href, "with length", css.length);
+            let style = dom.window.document.createElement("style");
+            style.setAttribute("origin", url.href);
+            style.innerHTML = css;
+            el.replaceWith(style);
+          }).catch(e => {
+            console.log("failed to get", url.href, e);
+          }));
+        } catch (e) {
+          console.log("assuming href is file path", path.resolve(path.dirname(file_list[i]), el.getAttribute("href")));
+          let style = dom.window.document.createElement("style");
+          style.innerHTML = fs.readFileSync(path.resolve(path.dirname(file_list[i]), el.getAttribute("href")), "utf8");
+          el.replaceWith(style);
+        }
+      }
+    })(el)));
+    await Promise.all(promise);
+    promise = [];
+    Array.from(dom.window.document.querySelectorAll("style")).forEach((el) => promise.push((el => {
+      // el.innerHTML = new cleanCSS({}).minify(el.innerHTML).styles;
+      return postcss([postcss_autoprefixer, postcss_handle_imports]).process(el.innerHTML, {
+        "from": file_list[i]
+      }).then(css => el.innerHTML = css);
+    })(el)));
+    await Promise.all(promise);
     Array.from(dom.window.document.querySelectorAll("script")).forEach(el => {
       if (el.hasAttribute("src")) {
         let src = el.getAttribute("src");
         el.removeAttribute("src");
         el.innerHTML = fs.readFileSync(path.resolve(file_list[i], "..", src), "utf8");
       }
-      el.innerHTML = uglifyes.minify(el.innerHTML, {
+      el.innerHTML = minifyes(el.innerHTML, {
         "compress": true,
         "mangle": true,
         "output": {
           "wrap_iife": true
         }
-      }).code;
+      });
+      el.setAttribute("hash", hash(el.innerHTML));
     });
     let name = path.parse(file_list[i]).name;
     let part_js = makeJS(name, dom.serialize().replace(/\s\s+?/g, ""), true, ++n);
     final_js += `/*${name}*/\n`;
     final_js += makeJS(name, dom.serialize().replace(/\s\s+?/g, ""), false, n) + ";\n";
-    fs.writeFileSync(path.resolve("./build/unpack", path.relative(path.resolve(process.cwd(), "./src"), file_list[i].replace(".html", ".js"))), uglifyes.minify(part_js).code);
+    fs.writeFileSync(path.resolve("./build/unpack", path.relative(path.resolve(process.cwd(), "./src"), file_list[i].replace(".html", ".js"))), minify(part_js));
   }
-  fs.writeFileSync(path.resolve("./build/pack/build.js"), uglifyes.minify(`(function(){${final_js}})()`, {
+  fs.writeFileSync(path.resolve("./build/pack/build.js"), `!function(){${minify(`(function(){${final_js}})()`, {
     "compress": true,
     "mangle": true,
     "output": {
       "wrap_iife": true,
       "comments": true
     }
-  }).code);
+  })}}();`);
   console.log("build finished...");
 }
+let id = 0;
 
 function makeJS(name, domstring, isInstance = false, n) {
+  id++;
   return `(function () {
     ${isInstance ? pack0 : ""}
-            const elements = document.createElement("template");
-            elements.setAttribute("for", ${
+            const elements$${id} = document.createElement("template");
+            elements$${id}.setAttribute("for", ${
     JSON.stringify(name)
     });
-            document.head.appendChild(elements);
-            elements.innerHTML = ${
+            document.head.appendChild(elements$${id});
+            elements$${id}.innerHTML = ${
     JSON.stringify(domstring).replace(/\\n/g, "")
             };
             customElements.define(${
@@ -97,7 +152,7 @@ function makeJS(name, domstring, isInstance = false, n) {
                 return ${n}
               }
               static get template(){
-                return elements;
+                return elements$${id};
               }
             });
           })()
--- tool/src/pack.0.js
+++ tool/src/pack.0.js
@@ -1,11 +1,13 @@
-this.__custom_fw__ = this.__custom_fw__ || {};
+var globalThis = globalThis || this || window;
+const _this = this || globalThis;
+_this.__custom_fw__ = _this.__custom_fw__ || {};
 let has_been_initialized = [];
-const app_state = this.__custom_fw__.state || new Map();
-this.__custom_fw__.state = app_state;
+const app_state = _this.__custom_fw__.state || new Map();
+_this.__custom_fw__.state = app_state;
 
 function mutationHandler(mutation) {
   let event = new Event("CustomElement" + mutation.type[0].toUpperCase() + mutation.type.substr(1), mutation);
-  event.target = mutation.target;
+  event.element = mutation.target;
   switch (mutation.type) {
     case "attributes":
       event.attributeName = mutation.attributeName;
@@ -18,22 +20,54 @@ function mutationHandler(mutation) {
   }
   mutation.target.dispatchEvent(event);
 }
-const mutationListener = this.__custom_fw__.mutation || new MutationObserver(function (mutations) {
+const mutationListener = _this.__custom_fw__.mutation || new MutationObserver(function (mutations) {
   mutations.forEach(mutationHandler);
 });
-this.__custom_fw__.mutation = mutationListener;
+_this.__custom_fw__.mutation = mutationListener;
+let elements = [];
+setInterval(() => {
+  let el = elements.shift();
+  if (!el.parentNode) {
+    if (el.__interval__) {
+      el.__interval__.forEach(el._clearinterval);
+    }
+    if (el.__timeout__) {
+      el.__timeout__.forEach(el._cleartimeout);
+    }
+    el.dispatchEvent(new Event("CustomElementDestroyed"));
+  }
+  elements.push(el);
+});
+window.script_map = new Map;
 
 function initialize(element, el_this, constructor, el_id) {
   mutationListener.observe(el_this, {
     attributes: true,
     childList: true
   });
+  elements.push(el_this);
   Array.from(element.querySelectorAll("script")).map(el => {
-    el.hasAttribute("init") ? (() => {
+    if (el.hasAttribute("init")) {
       if (has_been_initialized[el_id]) return -1;
       has_been_initialized[el_id] = true;
       new Function("constructor", "state", el.innerHTML)(constructor, app_state);
-    })() : new Function("self", "constructor", "root", "state", el.innerHTML)(el_this, constructor, el_this.shadowRoot, app_state);
+    } else {
+      let _settimeout = $setTimeout.bind(el_this);
+      let _setinterval = $setInterval.bind(el_this);
+      let _cleartimeout = $clearTimeout.bind(el_this);
+      let _clearinterval = $clearInterval.bind(el_this);
+      el_this._cleartimeout = _cleartimeout;
+      el_this._clearinterval = _clearinterval;
+      let func = null;
+      let hash = el.getAttribute("hash");
+      if (script_map.has(hash)) {
+        func = script_map.get(hash);
+      } else {
+        func = new Function("self", "constructor", "root", "state", "setTimout", "setInterval", "clearTimeout", "clearInterval", el.innerHTML);
+        script_map.set(hash, func);
+      }
+      func(el_this, constructor, el_this.shadowRoot, app_state, _settimeout, _setinterval, _cleartimeout, _clearinterval);
+    }
     el.parentNode.removeChild(el);
   });
 }
@@ -44,4 +78,30 @@ function init(el) {
   });
   sd.appendChild(el.constructor.template.content.cloneNode(true));
   initialize(sd, el, el.constructor, el.constructor.id);
+}
+
+function $setTimeout(func, delay) {
+  console.log(this);
+  this.__timeouts__ = this.__timeouts__ || [];
+  let id = setTimeout(func, delay);
+  this.__timeouts__.push(id);
+  return id;
+}
+
+function $setInterval(func, delay) {
+  console.log(this);
+  this.__interval__ = this.__interval__ || [];
+  let id = setInterval(func, delay);
+  this.__interval__.push(id);
+  return id;
+}
+
+function $clearInterval(id) {
+  console.log(this);
+  if (this.__interval__ && this.__interval__.includes(id)) this.__interval__.splice(this.__interval__.indexOf(id), 1);
+}
+
+function $clearTimeout(id) {
+  console.log(this);
+  if (this.__timeout__ && this.__timeout__.includes(id)) this.__timeout__.splice(this.__timeout__.indexOf(id), 1);
 }
